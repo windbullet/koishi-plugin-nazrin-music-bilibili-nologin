2d3aea9f20fc3877402dcb7854a2e4e51c52bb10
windbullet <2536810643@qq.com>
2024-03-07 11:55:29 +0800

    提交文件

--- readme.md
+++ readme.md
@@ -2,4 +2,4 @@
 
 [![npm](https://img.shields.io/npm/v/koishi-plugin-nazrin-video-bilibili-nologin?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-nazrin-video-bilibili-nologin)
 
-nazrin聚合搜索-bilibili支持（无需登录）
+nazrin聚合搜索-bilibili支持（无需登录，但画质较差）
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import { Context, Schema } from 'koishi'
+import {} from 'koishi-plugin-nazrin-core'
+
+export const inject = ['nazrin']
 
 export const name = 'nazrin-video-bilibili-nologin'
 
@@ -7,5 +10,50 @@ export interface Config {}
 export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
-  // write your plugin here
+  const thisPlatform = "bilibili"
+  ctx.nazrin.video.push(thisPlatform)
+
+  ctx.on("nazrin/video", async (ctx, keyword) => {
+    let result = await ctx.http.get(`https://api.xingzhige.com/API/b_search/?msg=${keyword}&n=15`)
+    let findList = []
+
+    if (result.code !== 0) {
+      findList = [
+        {
+          err: true,
+          platform: thisPlatform,
+        }
+      ]
+    } else {
+      for (let item of result.data) {
+        if (item.type === "video") {
+          findList.push({
+            name: item.video.title,
+            author: item.owner.name,
+            cover: item.video.cover,
+            url: item.video.url,
+            platform: thisPlatform,
+            err: false,
+          })
+        }
+      }
+    }
+    ctx.emit('nazrin/search_over', findList)
+  })
+
+  ctx.on('nazrin/parse_video', async (ctx, platform, url) => {
+    if (platform !== thisPlatform) {return}
+    let result = await ctx.http.get(`https://api.xingzhige.com/API/b_parse/?url=${url}`)
+    if (result.code !== 0) {
+      ctx.emit('nazrin/parse_error', result.msg)
+    } else {
+      let {data} = result
+      ctx.emit('nazrin/parse_over', 
+        data.video.url,
+        data.video.title,
+        data.owner.name,
+        data.video.fm,
+      )
+    }
+  })
 }
